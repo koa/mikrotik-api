3707b83272102f9b7ab0af4bb6be034c58c810c5
akoenig <andreas.koenig@berg-turbenthal.ch>
2025-04-07 08:04:14 +0200

    export QueryOperator

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,6 @@ pub mod simple;
 pub mod prelude {
     use crate::{device, protocol};
     pub use device::{MikrotikDevice, ParsedMessage};
-    pub use protocol::command::CommandBuilder;
+    pub use protocol::command::{CommandBuilder, QueryOperator};
     pub use protocol::word::{TrapCategory, TrapResult};
 }
